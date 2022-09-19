26750ce7e55ba85cf7a1fcaa34cf8feaae794536
Padias <matyas.krejza@seznam.cz>
2022-09-20 00:02:50 +0200

    change to http client (cors issue)

--- lib/src/core/provider/lines_provider.dart
+++ lib/src/core/provider/lines_provider.dart
@@ -1,16 +1,18 @@
 import 'dart:convert';
 
 import 'package:better_salina_app/src/core/model/salina_model.dart';
-import 'package:dio/dio.dart';
+import 'package:http/http.dart' as http;
 
 class LinesProvidel {
   Future<List<Salina>> loadLines(String stopId) async {
     try {
       List<Salina> salinas = [];
-      var response = await Dio()
-          .get('https://mapa.idsjmk.cz/api/Departures?stopid=$stopId');
-
-      List<dynamic> postLists = response.data["PostList"];
+      var client = http.Client();
+      final Uri url =
+          Uri.parse('https://mapa.idsjmk.cz/api/Departures?stopid=$stopId');
+      var response = await http.get(url);
+      final decodedResponse = jsonDecode(response.body);
+      List<dynamic> postLists = decodedResponse["PostList"];
       List<dynamic> departures = postLists[0]["Departures"];
       departures.forEach((it) {
         salinas.add(Salina.fromJson(it));
