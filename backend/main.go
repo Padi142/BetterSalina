626a34173c3e989369a5f6cd21cffbe68c8a5193
Padias <matyas.krejza@seznam.cz>
2022-09-20 17:24:16 +0200

    cors

--- backend/main.go
+++ backend/main.go
@@ -7,10 +7,20 @@ import (
 	"os"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
+
+	// Default config
+	app.Use(cors.New())
+
+	// Or extend your config for customization
+	app.Use(cors.New(cors.Config{
+		AllowOrigins: "*",
+		AllowHeaders: "Origin, Content-Type, Accept",
+	}))
 
 	app.Get("/saliny/:stopId", func(c *fiber.Ctx) error {
 		stopId := c.Params("stopId")
